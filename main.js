702e492b967715884f15f51a21cd223098424d40
ginlowl <de.tkpn23@gmail.com>
2021-07-19 20:39:42 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const btn = document.getElementById('omikuji');
+
+btn.addEventListener('click', function() {
+    const result = ['大吉','中吉','小吉','吉','凶','大凶']
+    const number = Math.floor(Math.random() * result.length);
+    btn.textContent = result[number];
+})
+// Math.random:0～0.99999999999
+// Math.floor:小数点切り捨て
